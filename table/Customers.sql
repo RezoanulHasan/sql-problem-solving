40cabb0ee830fcb792b047c8c9053d932223e118
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-11-03 02:45:51 +0600

    add customer table

---
+++ table/Customers.sql
@@ -0,0 +1,10 @@
+-- Create Customers table
+CREATE TABLE `Customers` (
+    `customer_id`  bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT INT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` varchar(50) NOT NULL,
+    `location` VARCHAR(100)NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+     `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+);
